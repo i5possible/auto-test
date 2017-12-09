765b64e6cb8e795ce7354d3abebde0779bcb990b
Liang Hong <lianghongbuaa@gmail.com>
2017-12-10 01:01:59 +0800

    structure adjust

--- src/main/java/com/kyeegroup/autotest/repository/ProductInfoRepository.java
+++ src/main/java/com/kyeegroup/autotest/repository/ProductInfoRepository.java
@@ -4,7 +4,9 @@ import com.kyeegroup.autotest.entity.ProductInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Created by lianghong on 09/12/2017.
+ *
+ * @author lianghong
+ * @date 09/12/2017
  */
 public interface ProductInfoRepository extends JpaRepository<ProductInfo, Integer> {
 
