aa89e31124867fdcc20aed26fc75df390654a191
Liang Hong <lianghongbuaa@gmail.com>
2017-12-10 23:22:37 +0800

    udpate test sql

--- src/main/resources/migration/mysql/V2__Test.sql
+++ src/main/resources/migration/mysql/V2__Test.sql
@@ -1,6 +1,6 @@
 INSERT INTO product_info VALUES (1, "json-server", 1, 1, "lianghong@kyeegroup.com", NULL, NULL);
 
 INSERT INTO test_case_detail
-VALUES (1, 1, "GetComments", "get", "", "", "", "", "", "http://localhost", "3000", "comments", NULL, NULL, NULL);
+VALUES (1, 1, "GetComments", "get", "", "", "", "", "", "http://localhost", "3000", "/comments/{id}", NULL, NULL, NULL);
 
 INSERT INTO test_case_triggered VALUES (1, 1, "2017-12-09 14:44:16", NULL, 0, "", 1, NULL, NULL, 1);
