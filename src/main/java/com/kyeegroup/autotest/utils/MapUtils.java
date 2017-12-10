037e8019299ca342b3b4c17a29c781e6a33dcfef
Liang Hong <lianghongbuaa@gmail.com>
2017-12-10 12:43:28 +0800

    update MapUtils using gson.from and TypeToken

--- src/main/java/com/kyeegroup/autotest/utils/MapUtils.java
+++ src/main/java/com/kyeegroup/autotest/utils/MapUtils.java
@@ -1,27 +1,18 @@
 package com.kyeegroup.autotest.utils;
 
-import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
-import com.kyeegroup.autotest.exception.IllegalJsonInputException;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * @author lianghong
  * @date 10/12/2017
  */
 public class MapUtils {
-    public static Map<String, String> toStringMap(String json) throws IllegalJsonInputException {
-        try {
-             return new JsonParser().parse(json)
-                    .getAsJsonObject()
-                    .entrySet().stream()
-                    .collect(Collectors.toMap( Map.Entry::getKey, set -> set.getValue().getAsString()));
-        } catch (JsonSyntaxException e) {
-            throw new IllegalJsonInputException("Illegal JsonSyntaxException");
-        } catch (Exception e) {
-            throw new IllegalJsonInputException("Illegal JsonInput!");
-        }
+
+    public static Map<String, String> jsonToMap(String json) {
+        return new Gson().fromJson(json, new TypeToken<HashMap<String, String>>() { }.getType());
     }
 }
--- src/test/java/com/kyeegroup/autotest/utils/MapUtilsTest.java
+++ src/test/java/com/kyeegroup/autotest/utils/MapUtilsTest.java
@@ -3,7 +3,6 @@ package com.kyeegroup.autotest.utils;
 import com.google.common.collect.Sets;
 import com.kyeegroup.autotest.exception.IllegalJsonInputException;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import java.util.Map;
 
@@ -17,26 +16,18 @@ public class MapUtilsTest {
 
     @Test
     public void covert_json_string_to_map() throws IllegalJsonInputException {
-        Map<String, String> map = MapUtils.toStringMap("{\n" +
+        Map<String, String> map = MapUtils.jsonToMap("{\n" +
                 "          \"title\": \"新人家去诶节日手指南，教你玩转APP\",\n" +
                 "          \"time\": \"2016.10.20-2016.12.1\",\n" +
                 "          \"type\": \"知识\",\n" +
                 "          \"img_url\": \"http://xxxx/Uploads/housephoto/253/252256/56dc035c55359.JPG\",\n" +
-                "          \"web_url\": \"xxx/xxx/xxx.html\"\n" +
+                "          \"web_url\": \"xxx/xxx/xxx.html\",\n" +
+                "           1: \"3\" " +
                 "        }");
 
-        assertThat(map.size()).isEqualTo(5);
-        assertThat(map.keySet()).isEqualTo(Sets.newHashSet("title", "time", "type", "img_url", "web_url"));
+        assertThat(map.size()).isEqualTo(6);
+        assertThat(map.keySet()).isEqualTo(Sets.newHashSet("1","title", "time", "type", "img_url", "web_url"));
         assertThat(map.get("type")).isEqualTo("知识");
-    }
-
-    @Test(expected = IllegalJsonInputException.class)
-    public void throw_exception_given_json_array() throws IllegalJsonInputException {
-        MapUtils.toStringMap("[\"123\",\"abc\"]");
-    }
-
-    @Test(expected = IllegalJsonInputException.class)
-    public void throw_exception_given_broken_json() throws IllegalJsonInputException {
-        MapUtils.toStringMap("{\"123\":\"abc\"]");
+        assertThat(map.get("1")).isEqualTo("3");
     }
 }
